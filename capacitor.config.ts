461d780a04aa3d512d044ffe05706d7d91f1bdbb
Masahiko Sakakibara <sakakibara@rdlabo.jp>
2022-06-15 19:01:42 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'stripe-ionic-react-sample',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
