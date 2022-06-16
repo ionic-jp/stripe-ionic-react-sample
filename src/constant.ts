4f77739abbbc2b719c4356b038071f71c42b2b20
Masahiko Sakakibara <sakakibara@rdlabo.jp>
2022-06-16 22:00:46 +0900

    chore(): termsを別変数に変更

--- src/constant.ts
+++ src/constant.ts
@@ -1,4 +1,6 @@
-export type IItem = {
+import {strict} from 'assert';
+
+type IItem = {
   id: number;
   title: string;
   subTitle: string;
@@ -28,3 +30,55 @@ export const Items: IItem[] = [
 真空パックにしていますので冷蔵庫で１週間程、日持ちいたします。`,
   },
 ];
+
+type ITerms = {
+  label: string;
+  value: string;
+}
+
+export const Terms: ITerms[] = [
+  {
+    label: '販売業者',
+    value: '一般社団法人リレーションデザイン研究所',
+  },
+  {
+    label: '代表責任者',
+    value: '榊原昌彦',
+  },
+  {
+    label: '所在地',
+    value: '〒530-0001 大阪府大阪市北区梅田1-1-3大阪駅前第3ビル 29階　1-1-1号室',
+  },
+  {
+    label: '電話番号',
+    value: '050-5240-6289',
+  },
+  {
+    label: '電話受付時間',
+    value: '10:00 〜 19:00',
+  },
+  {
+    label: 'メールアドレス',
+    value: 'info@rdlabo.jp',
+  },
+  {
+    label: '販売価格',
+    value: '各商品の紹介ページに記載している価格とします',
+  },
+  {
+    label: '商品代金以外に必要な料金',
+    value: '消費税',
+  },
+  {
+    label: '引き渡し時期',
+    value: 'ご注文から7日以内に対面販売',
+  },
+  {
+    label: 'お支払い方法とお支払いの時期',
+    value: 'クレジットカード決済：ご注文時にお支払いが確定します。',
+  },
+  {
+    label: '返品・交換・キャンセルについて',
+    value: '商品お渡し後の返品・交換・キャンセルには、基本的に対応しておりません。',
+  },
+]
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,5 +1,6 @@
 import { IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar, IonText } from '@ionic/react';
 import './Tab3.css';
+import { Terms } from '../constant'
 
 const Tab3: React.FC = () => {
   return (
@@ -16,50 +17,13 @@ const Tab3: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <IonList className="ion-padding-end ion-padding-bottom">
-          <IonItem lines="none">
-            <IonLabel position="stacked">販売業者</IonLabel>
-            <IonText>一般社団法人リレーションデザイン研究所</IonText>
-          </IonItem>
-          <IonItem lines="none">
-            <IonLabel position="stacked">代表責任者</IonLabel>
-            <IonText>榊原昌彦</IonText>
-          </IonItem>
-          <IonItem lines="none">
-            <IonLabel position="stacked">所在地</IonLabel>
-            <IonText>〒530-0001 大阪府大阪市北区梅田1-1-3大阪駅前第3ビル 29階　1-1-1号室</IonText>
-          </IonItem>
-          <IonItem lines="none">
-            <IonLabel position="stacked">電話番号</IonLabel>
-            <IonText>050-5240-6289</IonText>
-          </IonItem>
-          <IonItem lines="none">
-            <IonLabel position="stacked">電話受付時間</IonLabel>
-            <IonText>10:00 〜 19:00</IonText>
-          </IonItem>
-          <IonItem lines="none">
-            <IonLabel position="stacked">メールアドレス</IonLabel>
-            <IonText>info@rdlabo.jp</IonText>
-          </IonItem>
-          <IonItem lines="none">
-            <IonLabel position="stacked">販売価格</IonLabel>
-            <IonText>各商品の紹介ページに記載している価格とします。</IonText>
-          </IonItem>
-          <IonItem lines="none">
-            <IonLabel position="stacked">商品代金以外に必要な料金</IonLabel>
-            <IonText>消費税</IonText>
-          </IonItem>
-          <IonItem lines="none">
-            <IonLabel position="stacked">引き渡し時期</IonLabel>
-            <IonText>ご注文から7日以内に対面販売</IonText>
-          </IonItem>
-          <IonItem lines="none">
-            <IonLabel position="stacked">お支払い方法とお支払いの時期</IonLabel>
-            <IonText>クレジットカード決済：ご注文時にお支払いが確定します。</IonText>
-          </IonItem>
-          <IonItem lines="none">
-            <IonLabel position="stacked">返品・交換・キャンセルについて</IonLabel>
-            <IonText>商品お渡し後の返品・交換・キャンセルには、基本的に対応しておりません。</IonText>
-          </IonItem>
+          {
+            Terms.map(item => (
+              <IonItem lines="none">
+                <IonLabel position="stacked">{item.label}</IonLabel>
+                <IonText>{item.value}</IonText>
+              </IonItem>))
+          }
         </IonList>
       </IonContent>
     </IonPage>
