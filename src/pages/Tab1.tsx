6a00f91e9d797a8a9b46fdbdacc47c7cc4dd10f5
Masahiko Sakakibara <sakakibara@rdlabo.jp>
2022-06-19 11:11:24 +0900

    feat: アイテム一覧ページを作成

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,37 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonImg, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Tab1.css';
+import { Items } from '../constant';
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>商品リスト</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">商品リスト</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonList>
+          {Items.map((d, index) => (
+            <IonCard button={true} key={index}>
+              <div className="card-image">
+                <IonImg src={d.image} />
+              </div>
+              <IonCardHeader>
+                <IonCardSubtitle>{d.subTitle}</IonCardSubtitle>
+                <IonCardTitle>{d.title}</IonCardTitle>
+              </IonCardHeader>
+              <IonCardContent>
+                <p>¥{d.price.toLocaleString()}（税込）</p>
+              </IonCardContent>
+            </IonCard>
+          ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
