2e90549db17e3c2913a3a22e02568d5317ae104d
Masahiko Sakakibara <sakakibara@rdlabo.jp>
2022-06-19 11:13:48 +0900

    feat: カートページを作成

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,23 +1,116 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {
+  IonAvatar,
+  IonContent,
+  IonHeader,
+  IonImg,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonCheckbox,
+  IonText,
+  IonFooter,
+  IonButtons,
+  IonButton,
+  IonItemDivider,
+  useIonViewWillEnter,
+} from '@ionic/react';
 import './Tab2.css';
+import { Items } from '../constant';
+import { useMemo, useState } from 'react';
+
+type IChecked = {
+  val: number;
+  isChecked: boolean;
+};
 
 const Tab2: React.FC = () => {
+  useIonViewWillEnter(() => {
+    setRadioChecked(getRadioChecked());
+  });
+  const getRadioChecked = (): IChecked[] => {
+    return Items.map((d) => ({
+      val: d.id,
+      isChecked: localStorage.getItem(String(d.id)) === 'true',
+    }));
+  };
+  const [radioChecked, setRadioChecked] = useState(getRadioChecked());
+  const setChecked = (id: number, val: boolean) => {
+    localStorage.setItem(String(id), String(val));
+    setRadioChecked(getRadioChecked());
+  };
+
+  /**
+   * カート内商品の情報を計算
+   */
+  const { amount, quantity } = useMemo(() => {
+    const selectedItems: number[] = radioChecked
+      .filter((checked) => checked.isChecked)
+      .map((checked) => {
+        return Items.find((item) => item.id === checked.val)!.price | 0;
+      });
+    const amount = selectedItems.reduce((sum, element) => {
+      return sum + element;
+    }, 0);
+    return {
+      quantity: selectedItems.length,
+      amount,
+    };
+  }, [radioChecked]);
+
+  const submitPayment = async () => {
+  };
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>カート</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">カート</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <IonList>
+          <IonItem>
+            <IonLabel position="fixed">受取方法</IonLabel>
+            <IonText slot="end">対面受取</IonText>
+          </IonItem>
+          <IonItem lines="full">
+            <IonLabel position="fixed">決済方法</IonLabel>
+            <IonText slot="end">オンライン決済</IonText>
+          </IonItem>
+          <IonItemDivider>
+            <IonLabel>商品一覧</IonLabel>
+          </IonItemDivider>
+          {Items.map((d, i) => (
+            <IonItem key={i} lines={Items.length === i + 1 ? 'full' : undefined}>
+              <IonCheckbox slot="start" checked={radioChecked.find((check) => check.val === d.id)?.isChecked === true} onIonChange={(e) => setChecked(d.id, e.detail.checked)} />
+              <IonAvatar slot="start">
+                <IonImg src={d.image}></IonImg>
+              </IonAvatar>
+              <IonLabel>{d.title}</IonLabel>
+              <IonText slot="end">¥{d.price.toLocaleString()}</IonText>
+            </IonItem>
+          ))}
+        </IonList>
       </IonContent>
+      <IonFooter>
+        <IonToolbar color="primary">
+          <IonText slot="end" class="ion-margin-end ion-align-self-center">
+            {quantity}点 合計 ¥{amount}円（税込）
+          </IonText>
+          <IonButtons slot="end">
+            <IonButton fill="outline" onClick={submitPayment}>
+              決済する
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonFooter>
     </IonPage>
   );
 };
